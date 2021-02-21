a8313eb80e1c5bf31fc5ee1e1fe37be5581227a5
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-02-21 18:18:24 +0200

    update

--- api.js
+++ api.js
@@ -2,34 +2,31 @@ const fs = require("fs");
 const PORT=3000;
 console.log("server is running on port "+PORT)
 
-function createItem(text,priority,date,checked,id){
-   return {
-     myTodo: [{
-        text: text,
-        priority: priority,
-        date: date,
-        checked:checked
-        },
-      ],
-    id:Math.floor(Math.random()*100000000),
-}
-}
+
 const express = require('express');
 const e = require("express");
 const app = express();
 app.use(express.json());
 
+// function wait(res,req,next){
+//   setTimeout(()=>{
+//     next();
+//   },1000);
+// }
+// app.use(wait);
 app.get("/",(req,res)=>{
   try{
     fs.readdir("./database/",(err,files)=>{
-      let length=files.length;
-      if(length>0){
-        let latest=(JSON.parse(fs.readFileSync("./database/"+files[length-1],"utf-8")));
+      let id=files[files.length-1];
+      if(files.length>0){
+        let latest=(JSON.parse(fs.readFileSync("./database/"+id,"utf-8")));
         res.send(latest);
       }
-      if(length===0){
-        res({});
-        return;
+      if(files.length===0){
+        res.send({
+          myTodo:"",
+          id:0
+        });
       }
         console.log("list printed");
     })
@@ -42,9 +39,17 @@ app.get("/",(req,res)=>{
 app.get("/:id",(req,res)=>{
     const {id}= req.params;
     try{
+      if(id>0){
       let data=fs.readFileSync(`./database/${id}.json`,"utf-8");
       res.send(data);
-      console.log("item printed");
+      console.log("undo");
+      }
+      else{
+        res.send({
+          myTodo:[],
+          id:0
+        });
+      }
     }
     catch(e){
       res.send("no item with that id");
@@ -56,7 +61,15 @@ app.post("/", (req, res) => {
     try {
       if(body.id===null)
         body.id=0;
-      fs.writeFileSync(`./database/${body.id}.json`,JSON.stringify(body, null, 4));
+      else if(body.id<10&&body.id>0)
+        zero="000";
+      else if(body.id<100&&body.id>9)
+        zero="00";
+      else if(body.id<1000&body.id>99)
+        zero="0";
+      else
+        zero=0;
+      fs.writeFileSync(`./database/${zero+body.id}.json`,JSON.stringify(body, null, 4));
       res.status(201).send(`item ${body.id} added`);
     } catch (e) {
       res.status(500).json({ message: "Error!", error: e });
